849e6d594d7314c3f71483c12fda1138aacbda51
wanderer00 <s2017s39@e-mirim.hs.kr>
2018-08-18 18:42:25 +0900

    First commit

---
+++ app/src/main/java/s2017s39/kr/hs/mirim/s/MainActivity.java
@@ -0,0 +1,13 @@
+package s2017s39.kr.hs.mirim.s;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
